33122d868406b6857425f3bd00ce4199af1bd182
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-07-24 11:28:59 +0530

    Create defaultdatatype.java

---
+++ defaultdatatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+	static boolean defaultBoolean;
+	static byte defaultByte;
+	static char defaultChar;
+	static short defaultShort;
+	static int defaultInt;
+	static long defaultLong;
+	static float defaultFloat;
+	static double defaultDouble;
+	
+	public static void main(String[] args) {
+		System.out.println("Boolean	:" + defaultBoolean);
+		System.out.println("Byte	:" + defaultByte);
+		System.out.println("Char	:" + defaultChar);
+		System.out.println("Short	:" + defaultShort);
+		System.out.println("Int		:" + defaultInt);
+		System.out.println("Long	:" + defaultLong);
+		System.out.println("Float	:" + defaultFloat);
+		System.out.println("Double	:" + defaultDouble);
+	}
+}
