3843bd614e5a142aec93208a72adb595bbdea1b1
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-21 16:41:45 +0530

    Create swastikpattern.java

---
+++ swastikpattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class pattren {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c  = sc.nextInt();
+        int rmid = (r/2)+1;
+        int cmid = (r/2)+1;
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                if(j==0 && i<rmid)
+                    System.out.print("* ");
+                else if(i==0 && j>=cmid)
+                    System.out.print("* ");
+                else if(i==r-1 && j<cmid)
+                    System.out.print("* ");
+                else if(j==c-1 && i>=rmid)
+                    System.out.print("* ");
+                else if(i==rmid-1 || j==cmid-1)
+                    System.out.print("* ");
+                else 
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
