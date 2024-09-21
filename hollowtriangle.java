da519133dc2a365d671ad5eb294ab93fb417bf46
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-21 16:43:41 +0530

    Create hollowtriangle.java

---
+++ hollowtriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class hollowtriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<=i;j++) {
+                if(i==n-1 || j==0)
+                    System.out.print("* ");
+                else if(i==j)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
