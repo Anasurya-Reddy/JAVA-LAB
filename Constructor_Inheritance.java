229c731dbe4df0b0a9fd9144723023137932be9a
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-25 10:42:34 +0530

    Add files via upload

---
+++ Constructor_Inheritance.java
@@ -0,0 +1,25 @@
+class Vehical {
+	Vehical() {
+			System.out.println("This is a Vehical");
+		}
+}
+
+class FourWheeler extends Vehical {
+	FourWheeler() {
+			System.out.println("This is a FourWheeler");
+		}
+}
+
+class Car extends Vehical {
+	Car() {
+		System.out.println("This is a Car");
+	}
+}
+
+public class Constructor_Inheritance {
+	public static void main(String[] args) {
+		Vehical v = new Vehical();
+		FourWheeler f = new FourWheeler();
+		Car c = new Car();
+	}
+}
