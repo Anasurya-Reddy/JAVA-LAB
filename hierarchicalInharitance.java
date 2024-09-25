5fbe3520e32ac867cd3d3d1f89a3402e489fa2d4
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-25 10:17:00 +0530

    Add files via upload

---
+++ hierarchicalInharitance.java
@@ -0,0 +1,35 @@
+/*Hierarchical Inheritance-Abstract Class-Shape Hierarchy*/
+abstract class Shape {
+	abstract double getArea();
+}
+
+class Rectangle extends Shape {
+	double length;
+	double width;
+	Rectangle(double length,double width) {
+		this.length=length;
+		this.width=width;
+	}
+	double getArea() {
+		return length*width;
+	}
+}
+
+class Circle extends Shape {
+	double radius;
+	Circle(double radius) {
+		this.radius=radius;
+	}
+	double getArea() {
+		return 3.14*radius*radius;
+	}
+}
+
+public class hierarchicalInharitance {
+    public static void main(String[] args) {
+		Rectangle r = new Rectangle(10,20);
+		Circle c = new Circle(7);
+		System.out.println("Area of Rectangle: " + r.getArea());
+		System.out.println("Area of Circle: " + c.getArea());
+	}
+}
